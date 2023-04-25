ce9a42b6ffc94abfa9e5b8b71db3243d58adc93e
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-04-25 22:55:46 +0200

    chore: initialize frontend

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.scss"
+import { ConfigProvider } from "antd";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: `${inter.style.fontFamily}`,
+        },
+      }}
+    >
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </ConfigProvider>
+  );
+}

112cf2ddfb0cf33f591d5d5b6df81326d69e3e23
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-04-25 15:18:40 +0200

    chore: initialize backend

---
+++ backend/src/main/java/ftn/knowledge/engineering/ComputerRecommender/ComputerRecommenderApplication.java
@@ -0,0 +1,13 @@
+package ftn.knowledge.engineering.ComputerRecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerRecommenderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ftn/knowledge/engineering/ComputerRecommender/ComputerRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.knowledge.engineering.ComputerRecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

94fc9d8c054bdc18a5be2a67b39e5e042931b07b
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-05-03 23:12:26 +0200

    test

---
+++ backend/src/main/java/ftn/knowledge/engineering/ComputerRecommender/constants/FilePaths.java
@@ -0,0 +1,8 @@
+package ftn.knowledge.engineering.ComputerRecommender.constants;
+
+public class FilePaths {
+    public static String generalClasses = "data/ClassesPropertiesGeneral.rdf";
+    public static String cpuIndividuals = "data/cpu/Individuals_CPU.rdf";
+    public static String ramIndividuals = "data/GPU/GPU.rdf";
+    public static String ramClasses = "data/ram/ClassesPropertiesRAM.rdf";
+}
